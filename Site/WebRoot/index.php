3762253185f80bc98cc9c8df84b4c965bbb3e1b7
Ro5635 <ro5635@gmail.com>
2016-02-16 21:36:52 +0000

    GitHub Oddities
    GitHub localy has crashed out and caused issues…..

---
+++ Site/WebRoot/index.php
@@ -0,0 +1,41 @@
+<?php
+$pageTitle = 'Home';
+$pageDescription = 'Submit mesages to the Cube';
+
+
+require_once('../Includes/StdHead.php');
+
+echo '<span Class="Page"><section>';
+
+include('../Includes/StdImage.php');
+
+
+
+?>
+
+		<article id="MainPageContent">
+						<span id="PageTitleContainer"><span id="PageTitle"><h1>Main Page</h1></span></span>
+
+						<?php echo '<img src="' .  cloudFrontCannedPolicyURLSign('https://cdn.ro5635.co.uk/Cone.jpg') . '">'; 
+						
+
+
+						?>
+		</article>
+
+
+<?php
+echo '	</section></span>';
+include('../Includes/StdFooter.php');
+?>
+
+
+	
+	
+		
+
+		
+
+
+
+	
